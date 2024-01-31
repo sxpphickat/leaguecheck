b08efef14abe75ae8081336b62f25787e686ae70
sxpphickat <sphhhsh@gmail.com>
2024-01-31 13:05:41 -0300

    fetching from browser

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
 
 export default async function Home() {
- // console.log(data);
-  return <main>hi, {}</main>;
+  const data = await fetch('http://localhost:3000/api/get-player')
+  .then((res) => {
+    if(!res.ok) {
+      throw new Error('error no 666');
+      }
+      return res.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.error('Error: ', error);
+    });
+       
+    return <main>hi, {data.body.gameName}</main>;
 }

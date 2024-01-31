5c1a7e538d98da59b8113a90c17f3e2f1ff1d1c5
sxpphickat <sphhhsh@gmail.com>
2024-01-31 16:52:14 -0300

    feat: initial home

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,17 @@
 import "./globals.css";
+import { Inter } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="text-white">{children}</body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,12 @@
 
 export default async function Home() {
-  const data = await fetch('https://leaguecheck.vercel.app/api/get-player')
-  .then((res) => {
-    if(!res.ok) {
-      throw new Error('error no 666');
-      }
-      return res.json();
-    })
-    .then(data => {
-      return data;
-    })
-    .catch(error => {
-      console.error('Error: ', error);
-    });
-       
-    return <main>hi, {data.body.gameName}</main>;
+    return (
+      <div className="bg-neutral-950 h-screen w-screen flex flex-1 flex-col gap-4 justify-center items-center">
+        <h1 className="text-4xl">League Check</h1>
+        <form action="" className="flex justify-center">
+          <input className="border-none rounded-l-full px-4 py-1 w-4/5 bg-neutral-800 text-2xl outline-none" type="text" placeholder="Game Name + #BR1" />
+          <button className="border-l-black border-l-2 rounded-r-full px-4 py-1 bg-neutral-800 text-xl">CK</button>
+        </form>
+      </div>
+    );
 }

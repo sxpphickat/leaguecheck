d97b74dbf7b25696721a098930e750e57a3fd7d8
sxpphickat <sphhhsh@gmail.com>
2024-02-03 18:01:37 -0300

    feat: increase responsivity

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,16 +18,16 @@ export default function RootLayout({ children }) {
       <html lang="en" className={`${inter.className}`}>
         <body className="dark:text-white dark:bg-neutral-950 bg-white h-screen w-scree transition-all duration-500 
     ">
-          <header className="p-2 h-[5%] flex justify-start items-center">
+          <header className="p-2 h-12 flex justify-start items-center">
             <p>LEAGUE CHECK</p>
             <span className="ml-auto ">
               <DarkModeToggle />
             </span>
           </header>
-            <div className="h-[90%] border-y-2 dark:border-y-neutral-700 transition-all">
+            <div className="min-h-[700px] h-[90%] border-y-2 dark:border-y-neutral-700 transition-all">
               {children}
             </div>
-          <footer className="flex justify-center items-center p-2 h-[5%]">
+          <footer className="flex justify-center items-center p-2 h-12">
             <p>Made with ❤️ by <a className="text-blue-600" target="_blank" href="https://github.com/sxpphickat">@sxpphickat</a></p>
           </footer>
         </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
 'use client';
 import Head from "next/head";
 import SearchBar from "@/components/SeachBar";
+import { useEffect } from "react";
 
 export default function Home() {
-
     return (
       <>
-        <div className="flex flex-1 flex-col gap-8 justify-start items-center h-full">
-          <h1 className="text-4xl mt-72">LEAGUE CHECK</h1>
+        <div className="flex flex-1 flex-col gap-8 justify-center items-center h-full -mt-12">
+          <h1 className="text-4xl">LEAGUE CHECK</h1>
           <SearchBar />
         </div>
       </>

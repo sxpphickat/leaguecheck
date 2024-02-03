580ae815ec18d7173617a280ce3d79626df0b05d
sxpphickat <sphhhsh@gmail.com>
2024-02-03 12:29:05 -0300

    feat: add serverlist to searchbar

--- components/SeachBar.jsx
+++ components/SeachBar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import servers from '../public/riot_endpoints.json'
 
 export default function SearchBar() {
   const [ myPlaceholder, setMyPlaceholder ] = useState('Search');
@@ -19,10 +20,20 @@ export default function SearchBar() {
 
   }
 
+  function generateServerList() {
+    const servArr = [];
+    for (const key in servers['server']) {
+      servArr.push(
+        <option key={key} value={key}>{key}</option>
+      )
+    }
+    return servArr;
+  }
+
 	return (
 		<div className="">
-          <form action="" className="flex  justify-center rounded-xl text-black shadow-md dark:shadow-neutral-800 border-2 dark:border-neutral-700 border-neutral-200 bg-neutral-200 dark:bg-neutral-700 transition-all"
-              onMouseOver={() => setMyPlaceholder('Game Name + #BR1')}
+          <form action="" className="flex dark:text-white justify-center rounded-xl text-black shadow-lg dark:shadow-neutral-800 border-2 dark:border-neutral-700 border-neutral-200 bg-neutral-200 dark:bg-neutral-700 transition-all"
+              onMouseOver={() => setMyPlaceholder('Game Name + #Tag')}
               onMouseLeave={() => focused ? null : setMyPlaceholder('Search')}
               onFocus={() => setFocused(true)}
               onBlur={() => {
@@ -30,13 +41,16 @@ export default function SearchBar() {
               setFocused(false)
               }}
           >
-            <input className="dark:text-white rounded-l-xl px-4 py-1 dark:bg-neutral-800 text-2xl outline-none transition-all"
+            <select className="bg-neutral-100 dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-700 font-serif rounded-l-xl px-4 py-1 transition-all"
+                    name="server" id="server">
+                      {generateServerList()}
+            </select>
+            <input className="dark:text-white px-4 py-1 dark:bg-neutral-800 text-2xl outline-none transition-all"
                     type="text"
                     onChange={handleChange}
                     placeholder={myPlaceholder}
                   />
-            <button className=" dark:text-neutral-100 font-serif border-l dark:border-neutral-700 border-neutral-50 rounded-r-xl px-4 py-1 bg-neutral-100 dark:bg-neutral-900 text-2xl transition-all ">ck</button>
-{/*             <h1>{playerSearch}</h1> */}
+            <button className=" dark:text-neutral-100 font-serif border-l dark:border-neutral-700 hover:bg-neutral-200 dark:hover:bg-neutral-700 border-neutral-50 rounded-r-xl px-4 py-1 bg-neutral-100 dark:bg-neutral-900 text-2xl transition-all ">ck</button>
           </form>
         </div>
 	)
